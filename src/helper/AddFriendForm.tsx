5d59bd72b1c417f2786c62a68c7610b770b5b75f
Ahmed Moawad <ahmedmoawad550@gmail.com>
2025-01-04 11:29:46 +0200

    add arrowLeft

--- src/helper/AddFriendForm.tsx
+++ src/helper/AddFriendForm.tsx
@@ -1,10 +1,14 @@
+import { GoArrowLeft } from "react-icons/go";
+
 function AddFriendForm() {
   return (
     <section className="bg-white w-full md:w-[50%] lg:w-[30%] h-[100vh] px-6 py-8 flex flex-col gap-6 shadow-lg rounded-lg">
       {/* Header */}
-      <h1 className="text-2xl font-bold text-gray-800 text-center">
-        Start a New Chat
-      </h1>
+      <div className="flex items-center gap-4">
+        <GoArrowLeft className="text-xl text-gray-600 cursor-pointer" />
+        <h2 className="text-xl font-bold">New Chat</h2>
+      </div>
+      
 
       {/* Form */}
       <form className="flex flex-col gap-4">
