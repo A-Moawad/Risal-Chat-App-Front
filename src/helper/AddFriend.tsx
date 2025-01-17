64a38d1bae1e57505426c5ee31c2358a60046bce
Ahmed Moawad <ahmedmoawad550@gmail.com>
2025-01-17 08:13:03 +0200

    add tooltip on addFriendButton

--- src/helper/AddFriend.tsx
+++ src/helper/AddFriend.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { FaPlus } from "react-icons/fa";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 
 type AddFriendForm = {
   addFriendButtonClicked: boolean;
@@ -15,13 +20,22 @@ function AddFriend({
   };
 
   return (
-    <div
-      className="w-7 h-7 cursor-pointer bg-blue-500 flex justify-center items-center rounded-full hover:bg-blue-600 focus:outline focus:outline-blue-700"
-      onClick={handleAddFriendBtn}
-      aria-label="Add Friend"
-    >
-      <FaPlus className="text-white text-sm" />
-    </div>
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <div
+          className="w-7 h-7 cursor-pointer bg-blue-500 flex justify-center items-center rounded-full hover:bg-blue-600 focus:outline focus:outline-blue-700"
+          onClick={handleAddFriendBtn}
+          aria-label="Add Friend"
+        >
+          <FaPlus className="text-white text-sm" />
+        </div>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-[100px] h-[30px] p-1 flex items-center justify-center">
+        <p className="text-xs text-gray-700 text-center leading-tight">
+          Add friend
+        </p>
+      </HoverCardContent>
+    </HoverCard>
   );
 }
 
